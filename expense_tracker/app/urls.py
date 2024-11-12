5a0a5bfba03b4dfdbda4939872edb2a901cb9b2d
Marina <climovicimarina@gmail.com>
2024-11-12 15:58:41 +0100

    handled messages and logout method

--- expense_tracker/app/urls.py
+++ expense_tracker/app/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('signup', views.handle_signup, name="signup"),
-    path('success', TemplateView.as_view(template_name='success.html'), name='success'),
-    path('login', views.handle_login, name="login")
+    path("signup", views.handle_signup, name="signup"),
+    path("home", TemplateView.as_view(template_name='home.html'), name='home'),
+    path("login", views.handle_login, name="login"),
+    path("logout", views.handle_logout, name="logout"),
 ]
--- expense_tracker/app/views.py
+++ expense_tracker/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password #hash the password
 from .forms import UserForm, UserProfileForm, LoginForm
@@ -19,7 +19,7 @@ def handle_signup(request):
             profile.user = user
             profile.save()
 
-            return redirect('/success')
+            return redirect('/home')
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
@@ -35,20 +35,22 @@ def handle_login(request):
             password = login_form.cleaned_data["password"]
 
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
                 login(request, user)
                 request.session['logged_in'] = True
                 user_id = request.user.id
-                request.session['user_ud'] = user_id
-                messages.success(request, " Successfuly logged in!")
-
-                return redirect("/success")
+                request.session['user_id'] = user_id
+                return redirect("/home")
             else:
-                messages.error(request, " Invalid Credentials. Please try again")
-                return redirect("/login")
+                messages.error(request, "Invalid username or password. Please, try again.")
+                return render(request, "login.html", {"login_form": login_form})
 
     else:
         login_form = LoginForm()
 
     return render(request, "login.html", {"login_form": login_form})
+
+def handle_logout(request):
+    logout(request)
+    messages.success(request, "Successfuly logged out!")
+    return render(request, "logout.html")
